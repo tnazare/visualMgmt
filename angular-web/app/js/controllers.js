3dae0f2a99ad29f039d1db5e094841f07b259a31
Thibaut Nazare <tnazare@gmail.com>
2013-12-24 20:10:00 +0100

    Basic column changing and bootstrap integration

--- angular-web/app/js/controllers.js
+++ angular-web/app/js/controllers.js
@@ -2,10 +2,27 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+var visualMgmtControllers = angular.module('visualMgmt.controllers', []).
 
-  }])
-  .controller('MyCtrl2', [function() {
+    controller('BoardController', ['$scope', function($scope) {
 
-  }]);
+        $scope.list1 = {
+              title: 'AngularJS - Drag Me',
+              backlogDate:new Date(),
+              toDoDate:null,
+              wipDate:null,
+              toDeployDate:null,
+              testDate:null,
+              doneDate:null
+        };
+        $scope.list2 = {};
+        $scope.fromBackLogToToDo=function(item){
+            console.log("goes through here");
+            $scope.list2=$scope.list1;
+            $scope.list1={};
+        }
+        $scope.fromToDoToBackLog=function(item){
+            $scope.list1=$scope.list2;
+            $scope.list2={};
+        }
+    }]);
