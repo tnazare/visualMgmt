b9162c036101d6b1a2d324383fedaeda4b132c85
Thibaut Nazare <tnazare@gmail.com>
2014-02-25 14:43:01 +0100

    Basic JSON object Column representation and changing

---
+++ angular-web/app/js/classes/step.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+var Step = {
+
+    var list = [],
+
+    add = function(rank, item){
+        this.list[rank] = item;
+    },
+    remove = function(rank){
+        this.list[rank] = null;
+    }
+
+
+
+
+
+
+};
--- angular-web/app/js/controllers.js
+++ angular-web/app/js/controllers.js
@@ -5,24 +5,41 @@
 var visualMgmtControllers = angular.module('visualMgmt.controllers', []).
 
     controller('BoardController', ['$scope', function($scope) {
-
-        $scope.list1 = {
-              title: 'AngularJS - Drag Me',
-              backlogDate:new Date(),
-              toDoDate:null,
-              wipDate:null,
-              toDeployDate:null,
-              testDate:null,
-              doneDate:null
+        $scope. moveItemFromAColumnToAnother = function(item,taskList,destinationColumnName){
+            if(item !== undefined && taskList.length > 0){
+                if(destinationColumnName === "Todo"){
+                    item.toDoDate = new Date();
+                }
+                else{
+                    item.backlogDate = new Date();
+                }
+                item._currentColumn = destinationColumnName;
+            }
         };
-        $scope.list2 = {};
-        $scope.fromBackLogToToDo=function(item){
-            console.log("goes through here");
-            $scope.list2=$scope.list1;
-            $scope.list1={};
-        }
-        $scope.fromToDoToBackLog=function(item){
-            $scope.list1=$scope.list2;
-            $scope.list2={};
-        }
+        $scope.taskList = [{ _id: 1,
+                            _title: "Task0001",
+                            _currentColumn: "BackLog",
+                            _backlogDate: new Date(),
+                            _toDoDate: null,
+                            _wipDate: null,
+                            _toDeployDate: null,
+                            _testDate: null,
+                            _doneDate: null,
+                            _isInColumn: function(columnName){
+                                return this._currentColumn === columnName;
+                            }
+                         },
+                         {   _id: 2,
+                             _title: "Task0002",
+                             _currentColumn: "ToDo",
+                             _backlogDate: new Date(),
+                             _toDoDate: null,
+                             _wipDate: null,
+                             _toDeployDate: null,
+                             _testDate: null,
+                             _doneDate: null,
+                             _isInColumn: function(columnName){
+                                 return this._currentColumn === columnName;
+                             }
+                         }];
     }]);
